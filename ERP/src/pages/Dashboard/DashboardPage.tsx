99c6b5fd55715c6fc6c4c24e01cd6708d783a7c9
kdowon3 <dowon8849@gmail.com>
2025-03-23 22:02:02 +0900

    feat: components

--- ERP/src/pages/Dashboard/DashboardPage.tsx
+++ ERP/src/pages/Dashboard/DashboardPage.tsx
@@ -1,17 +1,8 @@
-import AlertCard from "../../components/common/AlertCard";
-
 const DashboardPage = () => {
 
     return (
         <div>
             <h1>대시보드</h1>
-            <AlertCard
-        productName="텀블러 (블랙)"
-        productCode="P0023"
-        stock={5}
-        avgSales={3}
-        orderDeadline="7일"
-      />
         </div>
     );
 }
