e24cbd2d1d458f7a3f461207df8c1f59cbb76786
yjbae3974 <brianbae6@gmail.com>
2025-12-25 12:41:56 +0900

    Feat: 전체 화면 모바일에서도 scrollable하도록 수정

--- ERP/src/layout.tsx
+++ ERP/src/layout.tsx
@@ -15,12 +15,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <div className='font-inter flex h-screen overflow-hidden bg-gray-50'>
-      <div className='flex flex-1'>
+      <div className='flex min-w-0 flex-1'>
         {!hideAuth && <Sidebar />}
         <main className='flex min-w-0 flex-1 flex-col'>
           {/* 헤더 */}
           {!hideAuth && (
-            <header className='flex h-16 items-center justify-between border-b border-gray-200 bg-white px-6 py-4'>
+            <header className='flex h-16 flex-shrink-0 items-center justify-between border-b border-gray-200 bg-white px-6 py-4'>
               <div>
                 <span className='text-xl leading-7 font-medium text-black'>스토어 ERP 시스템</span>
               </div>
@@ -34,7 +34,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           )}
 
           {/* 컨텐츠 */}
-          <section className='flex-1 overflow-auto p-3 sm:p-4 md:p-6'>{children}</section>
+          <section className='flex-1 overflow-auto p-3 sm:p-4 md:p-6'>
+            <div className='min-w-[1000px]'>{children}</div>
+          </section>
         </main>
       </div>
     </div>
